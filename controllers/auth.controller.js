e3bfd2b5a5e5991f6994877bea002c02e24470ba
TharHtet236401 <tharhteta3@gmail.com>
2024-09-02 21:21:49 +0700

    function/login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js"; 
 import bcrypt from "bcrypt"; // Import bcrypt for password hashing
-import { fMsg, generateCode, generateTokenAndSetCookie } from "../utils/libby.js"; // Import utility functions
+import { fMsg, generateCode, generateTokenAndSetCookie,decode,encode } from "../utils/libby.js"; // Import utility functions
 
 export const register = async (req, res, next) => { 
   try {
@@ -17,7 +17,7 @@ export const register = async (req, res, next) => {
 
     const inviteCode = generateCode(); // Generate invite code
 
-    const encryptedPassword = await bcrypt.hash(password, 10); // Hash password
+    const encryptedPassword = encode(password);// Hash password
     let user = await User.create({ // Create new user
       username,
       email,
@@ -36,3 +36,25 @@ export const register = async (req, res, next) => {
     next(error); 
   }
 };
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(new Error("User not found"));
+    }
+    
+    if (!decode(password, user.password)) {
+      return next(new Error("Invalid password"));
+    }
+    const returnUser = user.toObject();
+    delete returnUser.password;
+    delete returnUser.__v;
+    generateTokenAndSetCookie(res, user._id);
+
+    fMsg(res, "User logged in successfully", returnUser, 200);
+  }catch (error) {
+    next(error);
+  }
+}
