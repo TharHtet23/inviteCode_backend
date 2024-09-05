6f5210de1a268b9420aade912eba544016c68491
TharHtet236401 <tharhteta3@gmail.com>
2024-09-05 17:40:04 +0700

    set the origin  *

--- server.js
+++ server.js
@@ -12,19 +12,13 @@ import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
 import inviteCodeRoute from "./routes/inviteCode.route.js";
 
-const allowedOrigins = ['http://127.0.0.1:5500', 'https://tharhtet236401.github.io/inviteCode_frontEnd/','https://invite-code-front-end.vercel.app']; 
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, origin); 
-    } else {
-      callback(new Error('Not allowed by CORS')); 
-    }
-  },
+  origin:"*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
+
 
 app.use(express.json());
 app.use(cookieParser());
