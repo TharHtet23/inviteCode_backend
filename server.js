0782a3cd23ecb4eee2382766b27ca817a9b5219b
TharHtet236401 <tharhteta3@gmail.com>
2024-09-06 12:12:57 +0700

    migrate to one site

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import connectToMongoDB from "./config/db.js";
 import { fMsg } from "./utils/libby.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,17 +14,28 @@ import userRoute from "./routes/user.route.js";
 import inviteCodeRoute from "./routes/inviteCode.route.js";
 
 
+// Remove or comment out the existing CORS configuration
+// app.use(cors({
+//   origin:"https://tharhtet236401.github.io",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// }));
+
+// If you still want to use CORS for other origins, you can set it up like this:
 app.use(cors({
-  origin:"https://tharhtet236401.github.io",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: true, // This allows the server to accept requests from the same origin
   credentials: true,
 }));
-
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(express.static("public"));
+
+// Add this line to serve your main HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use("/test", testRoute);
 app.use("/api/auth", authRoute);
